e9d806897375dae37e3f30b1e19de661e4d61a7e
Brandon Hilkert <brandonhilkert@gmail.com>
2014-02-06 21:59:31 -0500

    Add caching

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'bundler'
 Bundler.require
 
+configure :production do
+  set :start_time, Time.now
+
+  before do
+    last_modified settings.start_time
+    etag settings.start_time.to_i
+  end
+end
+
 get '/' do
   erb :index
 end
